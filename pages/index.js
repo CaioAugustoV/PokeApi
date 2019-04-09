e9f12b373cf3240f7c0fa4f0e39dba5b3ca61b5b
Caio Augusto <caioaugustov@gmail.com>
2019-04-08 22:37:23 -0300

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import MainDesktop from '../compoments/pokedex/index'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <MainDesktop />
+      </div>
+    )
+  }
+}
+
